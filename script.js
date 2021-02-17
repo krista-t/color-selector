5742756029b4ae39858d5544c93f98bc6ff727ff
krista <krista.tomicic@gmail.com>
2021-02-17 16:10:14 +0100

    improve js code

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 "use strict";
 start();
-
+//just for listener
 function start() {
   const input = document.querySelector("input");
-  input.addEventListener("input", selectColor);
+  input.addEventListener("input", changeColor);
 }
 
-function selectColor() {
-  let hexValue = document.querySelector("input").value;
-  console.log(hexValue);
+//delegator function
+function changeColor() {
+  const hexValue = document.querySelector("input").value;
+  //call colorBox func
+  colorBox(hexValue);
+  //call  writeHex
   writeHex(hexValue);
+  //convert rgb to hex
+  const rgb = convertToRgb(hexValue);
+  //call writeTgB
+  writeRgB(rgb.r, rgb.g, rgb.b);
+  //convert rgb to hsl function call
+  const hsl = convertToHsl(rgb.r, rgb.g, rgb.b);
+  //write hsl
+  writeHsl(hsl.h, hsl.s, hsl.l);
+}
+
+function colorBox(colorValue) {
+  const colorBox = document.querySelector(".colorbox");
+  colorBox.style.background = colorValue;
 }
 
 function writeHex(value) {
-  console.log(value);
   let hexTxt = document.querySelector(".hex");
   hexTxt.innerHTML = `HEX: ${value}`;
-  convertToRgb(value);
 }
-
 function convertToRgb(hex) {
   let red = hex.substring(1, 3);
   let green = hex.substring(3, 5);
@@ -27,19 +40,17 @@ function convertToRgb(hex) {
   let r = parseInt(red, 16);
   let g = parseInt(green, 16);
   let b = parseInt(blue, 16);
-  const rgb = `${r} ${g} ${b}`;
-  writeRgB(rgb);
-  convertToHsl(r, g, b);
+  return { r, g, b };
 }
 
-function writeRgB(rgbvalue) {
-  console.log(rgbvalue);
+function writeRgB(red, blue, green) {
+  // console.log(red, blue, green);
   let rgbTxt = document.querySelector(".rgb");
-  rgbTxt.innerHTML = `RGB: ${rgbvalue}`;
+  rgbTxt.innerHTML = `RGB: ${red}, ${blue}, ${green}`;
 }
 
 function convertToHsl(r, g, b) {
-  console.log(r, g, b);
+  // console.log(r, g, b);
   r /= 255;
   g /= 255;
   b /= 255;
@@ -82,11 +93,11 @@ function convertToHsl(r, g, b) {
 
   console.log(h, s, l);
 
-  writeHsl(h, s, l);
+  return { h, s, l };
 }
 
 function writeHsl(hvalue, svalue, lvalue) {
   console.log(hvalue, svalue, lvalue);
   let hslTxt = document.querySelector(".hsl");
-  hslTxt.innerHTML = `HSL: ${hvalue} ${svalue} ${lvalue} `;
+  hslTxt.innerHTML = `HSL: ${hvalue} ${svalue}% ${lvalue}% `;
 }
